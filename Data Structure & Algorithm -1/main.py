2e2f0603a7e1efdd6f85eeb4363284cb29e95aee
Md-Mainul <mainulislamfaruqi@gmail.com>
2021-05-27 20:37:45 +0600

    Linked List

---
+++ Data Structure & Algorithm -1/main.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self, value):
+        self.next = None
+        self.prev = None
+        self.val = value
+
+
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def add(self, val):
+        node = Node(val)
+        if self.tail is None:
+            self.head = node
+            self.tail = node
+            self.size += 1
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+            self.size += 1
+
+    def __remove_node(self, node):
+        if node.prev is None:
+            self.head = node.next
+        else:
+            node.prev.next = node.next
+
+        if node.next is None:
+            self.tail = node.prev
+        else:
+            node.next.prev = node.prev
+        self.size -= 1
+
+    def remove(self, value):
+        node = self.head
+        while node is not None:
+            if node.val == value:
+                self.__remove_node(node)
+                break
+            node = node.next
+    def remove_last(self):
+        if self.tail is not None:
+            self.__remove_node(self.tail)
+
+    def __str__(self):
+        values = []
+        node = self.head
+        while node is not None:
+            values.append(node.val)
+            node = node.next
+        return f"[{', '.join(str(val) for val in values)}]"
+
+
+my_list = DoubleLinkedList()
+my_list.add(1)
+my_list.add(5)
+my_list.add(2)
+my_list.add(1)
+my_list.add(5)
+my_list.add(2)
+print(my_list)
+print(my_list)
+my_list.remove_last()
+print(my_list)
+my_list.remove_last()
+print(my_list)
+
